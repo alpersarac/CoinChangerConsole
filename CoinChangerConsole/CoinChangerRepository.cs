c5e387a2e3b27fcc858b1c4298b2a431c00eb795
alpersarac <alper.sarac42@gmail.com>
2023-08-31 21:22:22 +0300

    Repository Pattern Interface

---
+++ CoinChangerConsole/CoinChangerRepository.cs
@@ -0,0 +1,17 @@
+﻿using CoinChangerConsole.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinChangerConsole
+{
+    public class CoinChangerRepository : ICoinChangerRepository
+    {
+        public List<List<int>> GetListOfCombinations(int amoun, int[] banknotes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
