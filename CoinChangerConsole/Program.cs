24a63bc51c8ef807386b3708889187fc87075cac
alpersarac <alper.sarac42@gmail.com>
2023-09-01 13:06:16 +0300

    Getting the list of combinations

--- CoinChangerConsole/Program.cs
+++ CoinChangerConsole/Program.cs
@@ -10,6 +10,13 @@ namespace CoinChangerConsole
     {
         static void Main(string[] args)
         {
+            const int amount = 15;
+            int[] banknotes = new int[] { 25, 10, 5, 1 };
+
+            CoinChangerRepository coinChangerRepository = new CoinChangerRepository();
+            List<List<int>> combinations =  coinChangerRepository.GetListOfCombinations(amount, banknotes);
+
+            Console.ReadLine();
         }
     }
 }
--- CoinChangerConsole/Repository/CoinChangerRepository.cs
+++ CoinChangerConsole/Repository/CoinChangerRepository.cs
@@ -4,14 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CoinChangerConsole
 {
     public class CoinChangerRepository : ICoinChangerRepository
     {
-        public List<List<int>> GetListOfCombinations(int amoun, int[] banknotes)
+        public List<List<int>> GetListOfCombinations(int amount, int[] banknotes)
         {
-            throw new NotImplementedException();
+            List<List<int>> combinations= new List<List<int>>();
+            CreateListOfCombination(amount, banknotes, 0, new List<int>(), ref combinations);
+            return combinations;
         }
+        private void CreateListOfCombination(int amount, int[] banknotes, int nextBanknoteIndex, List<int> currentSenario, ref List<List<int>> combinations)
+        {
+            if (amount==0)
+            {
+                combinations.Add(new List<int>(currentSenario));
+                return;
+            }
+            for (int i = nextBanknoteIndex; i < banknotes.Length; i++)
+            {
+                if (amount >= banknotes[i])
+                {
+                    currentSenario.Add(banknotes[i]);
+                    CreateListOfCombination(amount- banknotes[i], banknotes,i,currentSenario,ref combinations);
+                    currentSenario.RemoveAt(currentSenario.Count-1);
+                }
+            }
+        }
+        
     }
 }
